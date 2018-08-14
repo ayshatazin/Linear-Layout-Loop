489225f697d6e580d44185b35ee9cf116c43f695
Tazin(Aunty) <ayshataz1996@gmail.c0m>
2018-08-14 23:38:58 +0600

    Linear Layout

---
+++ app/src/main/java/com/android/schemas/saferoad/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.schemas.saferoad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
